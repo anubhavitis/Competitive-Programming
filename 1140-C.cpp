bf8c94b0f4502473ab3b3ec003e58e20920f8e86
Anubhav Singhal <ss.lfsgd@gmail.com>
2019-03-23 23:31:24 +0530

    Add files via upload

---
+++ 1140-C.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+    int n;
+    cin>>n;
+    cout<<(n*(n-1)*(n+1))/3 -2<<"\n";
+
+}
