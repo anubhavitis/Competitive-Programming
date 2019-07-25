042247113dcb26c0ad5965a86a43cc97bc11a763
zeddie07 <ss.lfsgd@gmail.com>
2019-07-25 23:24:40 +0530

    Categorised all the programs uploaded till date.

---
+++ Educational Round 1140/1140-A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin>>n;
+	int sum=0,key=1,temp;
+	for(int i=1;i<=n;++i)
+    {
+        cin>>temp;
+        if(temp==i && key<=temp) sum++;
+        else if(temp>key) key=temp;
+    }
+    cout<<sum;
+}
