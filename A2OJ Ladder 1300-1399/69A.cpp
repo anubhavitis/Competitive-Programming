2b46beba9ec3c56b53bd18caf737d1c5039f6fcd
zeddie07 <ss.lfsgd@gmail.com>
2019-08-12 16:09:04 +0530

    361A solved

--- A2OJ Ladder 1300-1399/69A.cpp
+++ A2OJ Ladder 1300-1399/69A.cpp
@@ -22,18 +22,5 @@ int main()
     #endif
 
     IOS()
-    int n,k;
-    cin>>n>>k;
-    for (int i = 0; i < n; ++i)
-    {
-    	for (int j = 0; j < n; ++j)
-    	{
-    		if (i==j)
-    		{
-    			cout<<k<<" ";
-    		}
-    		else cout<<0<<" ";
-    	}
-    	cout<<"\n";
-    }
+    
 }
