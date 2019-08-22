033d900229ca4e3dd305090739bfcee4b141df5d
zeddie07 <ss.lfsgd@gmail.com>
2019-08-22 19:04:46 +0530

    B done

---
+++ Round 581 Div 2/A.cpp
@@ -0,0 +1,46 @@
+//Mark XIV
+#include<bits/stdc++.h>
+
+#define ll long long int
+#define mod %1000000007
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define deb(x) cerr<<#x<<" "<<x<<"\n";
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+void solve()
+{
+	string s;
+	cin>>s;
+	int pos=0,n=s.length();
+	for (int i = 1; i < n; ++i)
+	{
+		if(s[i]=='1') { pos=1; break;}
+	}
+	int ans=(n-1)/2;
+	if(pos || ans*2!=(n-1)) ans++;
+	cout<<ans<<"\n";
+}
+
+int main()
+{
+    //Skipped in presense of online judge.
+    #ifndef ONLINE_JUDGE
+    freopen("/home/zeddie/Documents/input.txt","r",stdin);
+    freopen("/home/zeddie/Documents/output.txt","w",stdout);
+    freopen("/home/zeddie/Documents/error.txt","w",stderr);
+    #endif
+
+    IOS()
+    int t=1;
+    //cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
---
+++ Round 581 Div 2/B.cpp
@@ -0,0 +1,42 @@
+//Mark XIV
+#include<bits/stdc++.h>
+
+#define ll long long int
+#define mod %1000000007
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define deb(x) cerr<<#x<<" "<<x<<"\n";
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+void solve()
+{
+	int n,l,r;
+	cin>>n>>l>>r;
+	ll mn=(n-l+1) + 2*( pow(2,(l-1)) - 1 );
+	ll mx=1 + 2*( pow(2,(r-1)) - 1 ) + pow(2,r-1)*(n-r);
+	cout<<mn<<" "<<mx;
+
+}
+
+int main()
+{
+    //Skipped in presense of online judge.
+    #ifndef ONLINE_JUDGE
+    freopen("/home/zeddie/Documents/input.txt","r",stdin);
+    freopen("/home/zeddie/Documents/output.txt","w",stdout);
+    freopen("/home/zeddie/Documents/error.txt","w",stderr);
+    #endif
+
+    IOS()
+    int t=1;
+    //cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
