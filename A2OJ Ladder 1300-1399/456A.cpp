1bad14e3a1d6545ba77e1bf4fa290081842e03b6
zeddie07 <ss.lfsgd@gmail.com>
2019-08-24 00:49:51 +0530

    456 A

---
+++ A2OJ Ladder 1300-1399/456A.cpp
@@ -0,0 +1,47 @@
+//Mark XIV
+#include<bits/stdc++.h>
+
+#define ll long long int
+#define mod %1000000007
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define deb(x) cerr<<#x<<" "<<x<<"\n";
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+void solve()
+{
+    int n,x,y;
+    cin>>n;
+    bool f=false;
+    for (int i = 0; i < n; ++i)
+    {
+        cin>>x>>y;
+        if(x!=y) f=true;
+    }
+    if(f) cout<<"Happy Alex";
+    else cout<<"Poor Alex";
+
+}
+
+int main()
+{
+    //Skipped in presense of online judge.
+    #ifndef ONLINE_JUDGE
+    freopen("/home/zeddie/Documents/input.txt","r",stdin);
+    freopen("/home/zeddie/Documents/output.txt","w",stdout);
+    freopen("/home/zeddie/Documents/error.txt","w",stderr);
+    #endif
+
+    IOS()
+    int t=1;
+    //cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
