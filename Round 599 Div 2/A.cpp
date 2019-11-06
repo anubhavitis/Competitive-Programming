4c4f05470653c4bc91032e0d70bccbdda1d390d5
zeddie07 <ss.lfsgd@gmail.com>
2019-11-06 20:50:32 +0530

    Round 599

---
+++ Round 599 Div 2/A.cpp
@@ -0,0 +1,55 @@
+//Mark XVII
+#include<bits/stdc++.h>
+
+#define ll long long int
+#define M 1000000007
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define deb(x) cerr<<#x<<" "<<x<<"\n";
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+void solve()
+{
+  int n;
+  cin>>n;
+  int a[n];
+  for (int i = 0; i < n; ++i)
+  {
+    cin>>a[i];
+  }
+  sort(a,a+n);
+ 
+  int k=1,ans=1;
+  for(int i=n-1; i>=0;--i)
+  {
+    if(k<=a[i]) { ans=max(k,ans); k++; }
+    else break;
+  }
+  cout<<--k;
+
+}
+
+int main()
+{
+  //Skipped in presense of online judge.
+  #ifndef ONLINE_JUDGE
+  freopen("/home/zeddie/Documents/input.txt","r",stdin);
+  freopen("/home/zeddie/Documents/output.txt","w",stdout);
+  freopen("/home/zeddie/Documents/error.txt","w",stderr);
+  #endif
+
+  IOS()
+  ll t=1;
+  cin>>t;
+  while(t--)
+  {
+    deb('\n')
+    solve();
+    cout<<"\n";
+  }
+}
