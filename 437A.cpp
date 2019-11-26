97f73d9278baeb27985e9d10c25e389589ff59d3
zeddie07 <ss.lfsgd@gmail.com>
2019-11-26 17:00:09 +0530

    437A

---
+++ 437A.cpp
@@ -0,0 +1,58 @@
+//Mark XVII
+#include<bits/stdc++.h>
+
+#define ll long long int
+#define M 1000000007
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define deb(x) cerr<<#x<<" "<<x<<"\n";
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+void solve()
+{
+  string ans,s;
+  int a[4];
+  for (int i = 0; i < 4; ++i)
+  {
+    cin>>s;
+    a[i]=s.length();
+    a[i]-=2;
+  }
+  for (int i = 0; i < 4; ++i)
+  {
+    bool f=true;
+    for (int j = 0; j < 4; ++j)
+    {
+      if(i==j) continue;
+      if(a[i]>a[j]/2 and a[i]<2*a[j]) f=false;
+    }
+    if(f) ans+=('A'+i);
+  }
+  if(ans.size()!=1) cout<<'C'; 
+  else cout<<ans;
+}
+
+int main()
+{
+  //Skipped in presense of online judge.
+  #ifndef ONLINE_JUDGE
+  freopen("/home/zeddie/Documents/input.txt","r",stdin);
+  freopen("/home/zeddie/Documents/output.txt","w",stdout);
+  freopen("/home/zeddie/Documents/error.txt","w",stderr);
+  #endif
+
+  IOS()
+  ll t=1;
+  //cin>>t;
+  while(t--)
+  {
+    solve();
+    cout<<"\n";
+    deb('\n')
+  }
+}
