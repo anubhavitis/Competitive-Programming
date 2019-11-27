507892b1f8bcb4ba59d0614cc1a19527db9cd977
zeddie07 <ss.lfsgd@gmail.com>
2019-11-27 19:52:48 +0530

    B done

--- Educationl Round 77 Div 2/C.cpp
+++ Educationl Round 77 Div 2/C.cpp
@@ -15,24 +15,10 @@ using namespace std;
 
 void solve()
 {
-  int a,b;
-  cin>>a>>b;
-  if(a>b)
-  {
-    int c=b;
-    b=a;
-    a=c;
-  }
-
-  if(b==2*a) { cout<<"YES"; return; }
-
-  int p=(2*b-a)/3;
-  b-=2*p;
-  a-=p;
-  if(a>b and a==2*b) { cout<<"YES"; return; }
-  if(a<b and b==2*a) { cout<<"YES"; return; }
-
-  else cout<<"NO";
+  int c,s;
+  cin>>c>>s;
+  ll sum=(s%c)*((s/c +1)*(s/c +1))+ (c-s%c)*((s/c)*(s/c));
+  cout<<sum;
 }
 
 int main()
