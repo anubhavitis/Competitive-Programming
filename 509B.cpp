d4692b960f44a02e10075fadac4647a30e72dcee
zeddie07 <ss.lfsgd@gmail.com>
2019-12-19 12:23:17 +0530

    Practice 509B done

--- 509B.cpp
+++ 509B.cpp
@@ -23,11 +23,11 @@ void solve()
 
   sort(all(b));
   int m2=ub(all(b),b[0])-b.begin();
-  if(k<b[n-1]-b[m2]+1) { cout<<"NO"; return; }
+  if(k<b[n-1]-b[m2-1]) { cout<<"NO"; return; }
   cout<<"YES\n";
   for(auto i:a)
   {
-    for (int j = 0; j < i; ++j) cout<<max(1,j-b[m2]+2)<<" ";
+    for (int j = 0; j < i; ++j) cout<<max(1,j-b[m2-1]+1)<<" ";
     cout<<"\n";
   }
 
