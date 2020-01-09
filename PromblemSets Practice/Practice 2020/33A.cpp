20db40506fd4dcf2a4ee84c5a9a1658928bf4666
zeddie07 <ss.lfsgd@gmail.com>
2020-01-09 18:42:22 +0530

    33A done.

---
+++ PromblemSets Practice/Practice 2020/33A.cpp
@@ -0,0 +1,60 @@
+//Mark XIX
+#include<bits/stdc++.h>
+
+#define big(x) greater<x>()
+#define ll long long int
+#define M 1000000007
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define deb(x) cerr<<#x<<" "<<x<<"\n";
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0);
+
+int temp;
+
+using namespace std;
+
+void solve()
+{
+  int n,m,k;
+  cin>>n>>m>>k;
+  int a[m+1]={};
+  for(int i=0;i<m+1;i++) a[i]=INT_MAX;
+  int t2;
+  for (int i = 0; i < n; ++i)
+  {
+    cin>>temp>>t2;
+    a[temp]=min(a[temp],t2);
+  }
+  for(int i=0;i<m+1;i++) cerr<<i<<" "<<a[i]<<"\n";
+  ll sum=0;
+  for (int i = 0; i < m; ++i)
+  {
+    deb(sum)
+    sum+=a[i+1];
+  }
+  sum>k?sum=k:1;
+  cout<<sum;
+}
+
+int main()
+{
+  //Skipped in presense of online judge.
+  #ifndef ONLINE_JUDGE
+  freopen("/home/zeddie/Documents/input.txt","r",stdin);
+  freopen("/home/zeddie/Documents/output.txt","w",stdout);
+  freopen("/home/zeddie/Documents/error.txt","w",stderr);
+  #endif
+
+  IOS()
+  ll t=1;
+  //cin>>t;
+  while(t--)
+  {
+    solve();
+    cout<<"\n";
+    deb('\n')
+  }
+}
