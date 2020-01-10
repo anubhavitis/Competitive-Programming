5f926200582c496a8a6f557a565c2cad35ac700c
zeddie07 <ss.lfsgd@gmail.com>
2020-01-10 07:51:15 +0530

    1141D done

--- PromblemSets Practice/Practice 2020/1141D.cpp
+++ PromblemSets Practice/Practice 2020/1141D.cpp
@@ -15,51 +15,39 @@
 int temp;
 
 using namespace std;
+map<char,std::vector<int> > m1,m2;
+std::vector<pair<int,int> > v;
+
+void calc(char c,char n)
+{
+  while(m1[c].size() and m2[n].size())
+  {
+    v.pb(mp(m1[c].back(),m2[n].back()));
+    m1[c].pop_back();
+    m2[n].pop_back();
+  }
+}
 
 void solve()
 {
   int n;
-  string s;
-  cin>>n>>s;
-  map< char,std::vector<int> > m1,m2;
-  for(int i=0;i<n;++i) m1[s[i]].pb(i+1);
-  cin>>s;
-  for(int i=0;i<n;++i) m2[s[i]].pb(i+1);
-  std::vector<int> v1,v2;
-  std::vector< pair<int,int> > ans;
+  string s,s2;
+  cin>>n>>s>>s2;
+  
+  for(int i=0;i<n;++i) m1[s[i]].pb(i+1),m2[s2[i]].pb(i+1);
+  
   for(auto i:m1)
   {
     if(i.first=='?') continue;
-    int x=min(m2[i.first].size(),i.second.size());
-    for (int ii = 0; ii < x; ++ii)
-      ans.pb(mp(i.second[ii],m2[i.first][ii]));
-    if(m2[i.first].size()>i.second.size())
-      for(int t=x;t<m2[i.first].size();++t) v2.pb(m2[i.first][t]);
-    else if(m2[i.first].size()<i.second.size())
-      for(int t=x;t<i.second.size();++t) v1.pb(i.second[t]);
+    calc(i.first,i.first);
+    calc(i.first,'?');
+    calc('?',i.first);
   }
+  for(auto i: m2) calc('?',i.first);
+  calc('?','?');
 
-  int i,j;
-  deb(ans.size())
-  deb(v1.size())deb(v2.size())
-  for(i=0;i<v1.size();i++)
-  {
-    if(m2['?'].size()==i) break;
-    ans.pb(mp(m2['?'][i],v1[i]));
-  }
-  for(j=0;j<v2.size();j++)
-  {
-    if(m1['?'].size()==j) break;
-    ans.pb(mp(m1['?'][j],v2[j]));
-  }
-  
-  while(i<m1['?'].size() and j<m2['?'].size()) 
-    ans.pb(mp(m1['?'][i++],m2['?'][j++]));
-
-  cout<<ans.size()<<"\n";
-  for(auto i:ans)
-    cout<<i.first<<" "<<i.second<<"\n";
-
+  cout<<v.size()<<"\n";
+  for(auto i:v) cout<<i.first<<" "<<i.second<<"\n";
 }
 
 int main()
