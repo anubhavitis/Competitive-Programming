cfa1e2873a973a966b3bb983bd6520536d269f5f
zeddie07 <ss.lfsgd@gmail.com>
2020-01-16 19:50:20 +0530

    New Challenge

---
+++ Educational Round 80 Div 2/B.cpp
@@ -0,0 +1,47 @@
+//Mark XIX
+#include<bits/stdc++.h>
+
+#define big(x) greater<x>()
+#define ll long long int
+#define M 1000000007
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define deb(x) cerr<<#x<<" "<<x<<"\n";
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0);
+
+int temp;
+
+using namespace std;
+
+void solve()
+{
+  ll a,b;
+  cin>>a>>b;
+  ll i=0;
+  while(b>=pow(10,i)-1) i++;
+  i--;
+  cout<<a*i;
+}
+
+int main()
+{
+  //Skipped in presense of online judge.
+  #ifndef ONLINE_JUDGE
+  freopen("/home/zeddie/Documents/input.txt","r",stdin);
+  freopen("/home/zeddie/Documents/output.txt","w",stdout);
+  freopen("/home/zeddie/Documents/error.txt","w",stderr);
+  #endif
+
+  IOS()
+  ll t=1;
+  cin>>t;
+  while(t--)
+  {
+    solve();
+    cout<<"\n";
+    deb('\n')
+  }
+}
