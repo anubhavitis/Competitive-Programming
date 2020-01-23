0a70d2c48428c9d59a02f237832dbd7136a65bcf
zeddie07 <ss.lfsgd@gmail.com>
2020-01-23 16:13:14 +0530

    96B done

---
+++ 01- PromblemSets Practice/Practice 2020/96B.cpp
@@ -0,0 +1,58 @@
+//Mark XIX
+#include<bits/stdc++.h>
+
+#define big(x) greater<x>()
+#define ll long long int
+#define M 1000000007
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define deb(x) cerr<<#x<<" "<<x<<"\n";
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0);
+
+int t1,t2;
+
+using namespace std;
+
+void solve()
+{
+  string s;
+  cin>>s;
+  int n=s.size();
+  string ans;
+  n-=n/2;
+  while(n--) ans+="47";
+  sort(all(ans));
+  do
+  {
+    if(stoll(ans)>=stoll(s))
+    {
+      cout<<ans;
+      return;
+    }
+  }while(next_permutation(all(ans)));
+  cout<<'4'<<ans<<'7';
+
+}
+
+int main()
+{
+  //Skipped in presense of online judge.
+  #ifndef ONLINE_JUDGE
+  freopen("/home/zeddie/Documents/input.txt","r",stdin);
+  freopen("/home/zeddie/Documents/output.txt","w",stdout);
+  freopen("/home/zeddie/Documents/error.txt","w",stderr);
+  #endif
+
+  IOS()
+  ll t=1;
+  //cin>>t;
+  while(t--)
+  {
+    solve();
+    cout<<"\n";
+    deb('\n')
+  }
+}
