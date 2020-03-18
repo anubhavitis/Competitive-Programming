ac789846a99d3f773f5d956f33861024ca478634
zeddie07 <ss.lfsgd@gmail.com>
2020-03-18 18:26:00 +0530

    Back to practice

--- 01- PromblemSets Practice/Practice 2020/940B.cpp
+++ 01- PromblemSets Practice/Practice 2020/940B.cpp
@@ -3,7 +3,7 @@
 
 #define big(x) greater<x>()
 #define ll long long int
-#define M 998244353
+#define M 1000000007
 #define mp make_pair
 #define pb push_back
 #define lb lower_bound
@@ -11,41 +11,29 @@
 #define all(x) x.begin(), x.end()
 #define deb(x) cerr<<#x<<" "<<x<<"\n";
 #define IOS() ios_base::sync_with_stdio(0);cin.tie(0);
-#define rep(a,b,c) for(int i=a; i<b; i+=c)
+#define rep(i,a,b) for(int i=a; i<b; ++i)
+
 using namespace std;
-ll power(ll x, ll y) 
-{ 
-  ll res = 1;
-  x = x % M;
-  while (y > 0) 
-  { 
-    if (y & 1) 
-      res = (res*x) % M; 
-    y = y>>1;
-    x = (x*x) % M; 
-  } 
-  return res; 
-} 
 
 void solve()
 {
-  int n,m;
-  cin>>n>>m;
-  ll f[200001]={};
-  f[0]=1;
-  rep(1,200001,1) f[i]=(i*f[i-1])%M;
+  unsigned ll n,k,a,b;
+  cin>>n>>k>>a>>b;
+  
+  if(n<k or k==1) { cout<<(n-1)*a; return; }
+  unsigned ll cnt=0;
+  while(n>k)
+  {
+    if(n%k) cnt+=a*(n%k),n-=(n%k);
+    if(n==k) break;
+    cnt+=min((n-n/k)*a,b);
+    n/=k; 
+  }
 
-  ll c1=( f[m]* power(f[n-1], M-2) % M * power(f[m-n+1], M-2) % M ) % M ;
-  deb(c1)
-  ll c2=power(2,n-3);
-  deb(c2)
-  ll c3=(n-2);
-  deb(c3)
-  cout<<( ( (c1*c2)%M )*c3 )%M;
+  if(n==k) cnt+=min((n-1)*a,b);
+  else cnt+=(n-1)*a;
 
-
-
-
+  cout<<cnt;
 }
 
 int main()
