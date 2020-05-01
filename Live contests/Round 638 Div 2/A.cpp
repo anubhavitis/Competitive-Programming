24c521521e149f449c3e59b2e0ae93e46ca972d3
Anubhav Singhal <ss.lfsgd@gmail.com>
2020-05-01 19:56:18 +0530

    New Challenge!

---
+++ Live contests/Round 638 Div 2/A.cpp
@@ -0,0 +1,41 @@
+//Mark XXVI
+#include<bits/stdc++.h>
+
+#define big(x) greater<x>()
+#define ll long long int
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define deb(x) cerr<<#x<<" : "<<x<<"\n";
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0);
+#define rep(i,b,c) for(i=b; i<c; ++i)
+#define rrep(i,b,c) for(i=b; i>=c; --i)
+
+#define M 998244353
+#define LINF 1e18
+#define INF INT_MAX
+int i,j;
+using namespace std;
+
+void solve()
+{
+  
+}
+
+int main()
+{
+  //Skipped in presense of online judge.
+  #ifndef ONLINE_JUDGE
+  freopen("/home/zeddie/Documents/input.txt","r",stdin);
+  freopen("/home/zeddie/Documents/output.txt","w",stdout);
+  freopen("/home/zeddie/Documents/error.txt","w",stderr);
+  #endif
+
+  IOS()
+  ll t=1;
+  //cin>>t;
+  while(t--)
+    solve();
+}
