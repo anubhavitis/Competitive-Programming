804fdc3a868adeead36a646ab135401687016f47
Anubhav Singhal <ss.lfsgd@gmail.com>
2020-06-13 20:30:00 +0530

    Round 649 begins

---
+++ Live contests/Round 649 Div 2/A.cpp
@@ -0,0 +1,43 @@
+//Mark XXVII
+#include<bits/stdc++.h>
+
+#define big(x) greater<x>()
+#define ll long long int
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define PI 3.14159265
+#define deb(x) cerr<<#x<<" : "<<x<<"\n";
+#define deball(x) for(auto iit:x) cerr<<iit<<" ";cerr<<"\n";
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0);
+#define rep(i,b,c) for(i=b; i<c; ++i)
+#define rrep(i,b,c) for(i=b; i>=c; --i)
+
+#define M 998244353
+#define LINF 1e18
+#define INF INT_MAX
+using namespace std;
+
+ll i,j,n,m;
+ 
+void solve()
+{
+}
+
+int main()
+{
+  //Skipped in presense of online judge.
+  #ifndef ONLINE_JUDGE
+  freopen("/home/zeddie/Documents/input.txt","r",stdin);
+  freopen("/home/zeddie/Documents/output.txt","w",stdout);
+  freopen("/home/zeddie/Documents/error.txt","w",stderr);
+  #endif
+
+  IOS()
+  ll t=1;
+  // cin>>t;
+  while(t--)
+    solve();
+} 
