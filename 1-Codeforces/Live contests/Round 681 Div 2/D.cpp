937a9d1cf6a72f9898aeab0e913620496e21cf8e
anubhavitis <ss.lfsgd@gmail.com>
2020-11-13 19:59:17 +0530

    New Challenge

---
+++ 1-Codeforces/Live contests/Round 681 Div 2/D.cpp
@@ -0,0 +1,70 @@
+//Mark XXXII
+#include<bits/stdc++.h>
+
+#define ll             long long
+#define mp              make_pair
+#define pb              push_back
+#define lb              lower_bound
+#define ub              upper_bound
+#define all(x)          x.begin(), x.end()
+#define big(x)          greater<x>()
+
+#define sp              fixed<<setprecision
+#define vi              vector<int>
+#define vvi             vector<vi>
+#define pi              pair<int,int>
+
+#define PI              3.14159265
+#define M               1000000007
+#define LINF            LONG_MAX
+#define NL              LONG_MIN
+#define INF             INT_MAX
+#define NI              INT_MIN
+
+#define IOS()           ios_base::sync_with_stdio(0);cin.tie(0);
+#define deb(x)          cerr<<#x<<" : "<<x<<"\n";
+#define deball(x)       for(auto iit:x) cerr<<" "<<iit;cerr<<"\n";
+#define rep(i,b,c)      for(i=b; i<c; ++i)
+#define rrep(i,b,c)     for(i=b; i>=c; --i)
+
+using namespace std;
+
+//Code begins from here!!
+
+#define mxn 1000
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(auto &it: v) cin>>it;
+
+    ll k=0;
+    for(int i=n-2; i>=0; --i){
+        if(v[i]-k<0){
+            cout<<"NO\n";
+            return;
+        }
+
+        if(v[i]<=v[i+1]) continue;
+        k+=v[i]-v[i+1];
+    }
+
+    cout<<"YES\n";
+    
+}
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+    freopen ( "/home/zeddie/Documents/input.txt", "r", stdin );
+    freopen ( "/home/zeddie/Documents/output.txt", "w", stdout );
+    freopen ( "/home/zeddie/Documents/error.txt", "w", stderr );
+#endif
+    IOS()
+    int t = 1;
+    cin >> t;
+
+    for (int i = 0; i < t; ++i)
+        solve();
+}
